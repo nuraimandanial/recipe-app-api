130aaf63ffc8ff8816f076f8d6ef4f3bee63785b
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-04-13 21:22:20 +0800

    Day 2 - Created Tags API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -9,11 +9,9 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
-
 
 def create_user(**params):
     """Create and return a new user."""
